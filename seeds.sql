2c48696646e372e633a4786f69c4dd01a6d812ce
ctvb <deenicks@hotmail.com>
2023-04-14 01:49:55 -0500

    schema and seeds made

---
+++ seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES ("Human Resources"),
+    ("Sales"),
+    ("IT"),
+    ("Payroll"),
+
+INSERT INTO role (title)
+VALUES ("HR Drone"),
+    ("HR Manager"),
+    ("Sales Bro"),
+    ("Head of Sales"),
+    ("IT Person"),
+    ("IT Enthusiast"),
+    ("Lord of Money"),
+
+INSERT INTO employee(first_name, last_name)
+VALUES ("Dan"),
+    ("Rachel"),
+    ("Bob"),
+    ("Alex"),
+    ("Michelle"),
+    ("Juan"),
+    ("Nicola"),
+    ("Robin"),
+    ("Steve"),
+    ("Elvis"),
